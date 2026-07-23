d019fce9e4709f36e3eaccc5a88c50e2f91ebc5a
Timothy Johnson <timothy.johnson@broadcom.com>
2026-07-23 18:21:32 -0400

    Use ibm-clang in sample swig project
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- native/python/swig/sample/zmetal.h
+++ native/python/swig/sample/zmetal.h
@@ -3,7 +3,7 @@
 #ifndef ZMETAL_H
 #define ZMETAL_H
 
-#if defined(__cplusplus) && (defined(__IBMCPP__) || defined(__IBMC__))
+#if defined(__cplusplus) && defined(__MVS__)
 extern "OS"
 {
 #elif defined(__cplusplus)
