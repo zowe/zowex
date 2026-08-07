13ee54d014e33537c862cf010a97ca956d5bfe23
CBforZ <chris.boehm@broadcom.com>
2026-08-07 15:04:50 -0400

    remove unused import
    Signed-off-by: CBforZ <chris.boehm@broadcom.com>

--- packages/sdk/vitest.config.ts
+++ packages/sdk/vitest.config.ts
@@ -16,7 +16,7 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import rootConfig from "../../vitest.config";
-const { projects, ...sharedTestConfig } = rootConfig.test || {};
+const { ...sharedTestConfig } = rootConfig.test || {};
 const sharedConfig = { ...rootConfig, test: sharedTestConfig };
 
 export default mergeConfig(
