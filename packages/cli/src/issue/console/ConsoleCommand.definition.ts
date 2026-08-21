ab9e64a79b847aea1fa7650d16f3ed4bd784d21a
Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
2026-08-20 23:02:39 -0400

    lint
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>

--- packages/cli/src/issue/console/ConsoleCommand.definition.ts
+++ packages/cli/src/issue/console/ConsoleCommand.definition.ts
@@ -36,7 +36,8 @@ export const ConsoleCommand: ICommandDefinition = {
         {
             name: "console-name",
             aliases: ["cn"],
-            description: "Specify the console name of the console to issue the command to. Defaults to the current user ID.",
+            description:
+                "Specify the console name of the console to issue the command to. Defaults to the current user ID.",
             type: "string",
             required: false,
         },
